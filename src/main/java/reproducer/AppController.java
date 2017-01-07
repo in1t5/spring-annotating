9109181aa19a4a58ea7aafd872526de26498c741
in1t5 <inithandshake@googlemail.com>
2017-01-07 15:41:34 +0100

    coveralls now maybe working

--- src/main/java/reproducer/AppController.java
+++ src/main/java/reproducer/AppController.java
@@ -9,9 +9,8 @@ public class AppController {
 
 	private static ApplicationContext ctx;
 
-	public static void main(String[] args) {
+	public String init() {
 		ctx = new AnnotationConfigApplicationContext(AppConfig.class);
-		System.out.println(ctx.getBean("A", Command.class).exec());
-		System.out.println(ctx.getBean("B", Command.class).exec());
+		return ctx.getBean("A", Command.class).exec();
 	}
 }
--- src/test/java/reproducer/TravisCiMavenTest.java
+++ src/test/java/reproducer/TravisCiMavenTest.java
@@ -1,12 +1,15 @@
 package reproducer;
 
-import org.junit.Assert;
 import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TravisCiMavenTest {
 
 	@Test
 	public void testTravisCi() {
-		Assert.assertTrue(true);
+		AppController controller = new AppController();
+		String response = controller.init();
+
+		assertThat(response).isNotBlank().isEqualTo("A");
 	}
 }
